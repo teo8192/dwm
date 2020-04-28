2c68be847c4ac5764cbb9894be13d80bbd85c251
Teo <teodor@dahlknutsen.no>
2020-04-28 20:16:52 +0200

    Use the best gray

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#262626";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
@@ -32,8 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Discord",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "bochs",    NULL,       NULL,       0,			0,           -1 },
+	{ "Discord",  NULL,       NULL,       1 << 1,       0,           0 },
 };
 
 /* layout(s) */
@@ -50,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -64,6 +63,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; //[> component of dmenucmd, manipulated in spawn() <]
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *plum_sel[] = { "plum", "prim", NULL };
+static const char *kill_sel[] = { "tabc.sh", "kill", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -110,12 +111,14 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         Mod4Mask,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         Mod4Mask,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         Mod4Mask,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkClientWin,			MODKEY,			Button1,		spawn,			{.v = plum_sel} },
+	{ ClkClientWin,			MODKEY,			Button3,		spawn,			{.v = kill_sel} },
 };
 
