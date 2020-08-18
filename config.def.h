950ac2099cf8774e37c06fa5758e11da6bfea56a
Teo <teodor@dahlknutsen.no>
2020-08-18 14:19:37 +0200

    Change some keybinds

--- config.def.h
+++ config.def.h
@@ -107,18 +107,18 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         Mod4Mask,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         Mod4Mask,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         Mod4Mask,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkClientWin,			MODKEY,			Button2,		spawn,			{.v = plum_sel} },
-	{ ClkClientWin,			MODKEY,			Button3,		spawn,			{.v = kill_sel} },
+	{ ClkLtSymbol,          0,                  Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,                  Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,                  Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,                  Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         Mod4Mask,			Button1,        movemouse,      {0} },
+	{ ClkClientWin,         Mod4Mask|ShiftMask, Button2,        togglefloating, {0} },
+	{ ClkClientWin,         Mod4Mask,           Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,					Button1,        view,           {0} },
+	{ ClkTagBar,            0,                  Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,             Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,             Button3,        toggletag,      {0} },
+	{ ClkClientWin,			MODKEY,		        Button2,		spawn,			{.v = plum_sel} },
+	/* { ClkClientWin,			MODKEY,			Button3,		spawn,			{.v = kill_sel} }, */
 };
 
