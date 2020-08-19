28ffe3329dfc1657e10d75c4f05a7ef81d8010da
Teo <teodor@dahlknutsen.no>
2020-08-19 20:09:14 +0200

    do not spawn terminal when middle clicking topbar

--- config.def.h
+++ config.def.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; //[> component of dmenucmd, manipulated in spawn() <]
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+/* static const char *termcmd[]  = { "st", NULL }; */
 static const char *plum_sel[] = { "xplum", "prim", NULL };
 static const char *kill_sel[] = { "tabc.sh", "kill", NULL };
 
@@ -110,7 +110,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,                  Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,                  Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,                  Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,                  Button2,        spawn,          {.v = termcmd } },
+	/* { ClkStatusText,        0,                  Button2,        spawn,          {.v = termcmd } }, */
 	{ ClkClientWin,         Mod4Mask,			Button1,        movemouse,      {0} },
 	{ ClkClientWin,         Mod4Mask|ShiftMask, Button2,        togglefloating, {0} },
 	{ ClkClientWin,         Mod4Mask,           Button3,        resizemouse,    {0} },
