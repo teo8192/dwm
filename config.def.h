7997ea0c32ec7b11527a6c0f40b609d26ce6edf8
Teo <teodor@dahlknutsen.no>
2020-08-20 22:48:20 +0200

    Fix class names

--- config.def.h
+++ config.def.h
@@ -36,8 +36,8 @@ static const Rule rules[] = {
 	/* { "Discord",  NULL,       NULL,       1 << 1,       0,           0 }, */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-/* +	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 }, */
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "discord", NULL,     NULL,           1 << 1,    0,          0,           0,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
